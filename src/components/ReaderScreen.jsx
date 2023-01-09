72346c8e125eb4e1da1580c7bb2e0a55c78c5d48
Emmanuel Essien <ehmaessien@gmail.com>
2023-01-09 13:11:49 +0100

    Create ReaderScreen component

--- src/components/ReaderScreen.jsx
+++ src/components/ReaderScreen.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import '../style/reader/ReaderScreen.css'
+import React from "react";
+import "../style/reader/ReaderScreen.css";
+
+const data = [
+  {
+    id: 1,
+    title: "Alice in Wonderland",
+    author: "Wade Warren",
+    chapters: [],
+  },
+];
 
 function ReaderScreen() {
-	return (
-		<>
-			<div className="container mx-auto">
-				<div>
-					<div>
-						<h2>Story name- </h2>
-					</div>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="container mx-auto">
+        {data.map((item) => (
+          <div>
+            <div>
+              <h2>Story name- <span>{item.title}</span></h2>
+			  <div>
+				  
+			  </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default ReaderScreen
+export default ReaderScreen;
